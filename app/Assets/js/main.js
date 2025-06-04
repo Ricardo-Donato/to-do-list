997192d115db440aaf52c543e4b87b9913582a76
Pedro Ricardo Donato da SIlva <pdonatodasilva@gmail.com>
2025-06-04 16:57:31 -0300

    Ajustes no HTML e CSS + funcionalidade prévia de adição de tarefa

--- app/Assets/js/main.js
+++ app/Assets/js/main.js
@@ -1,2 +1,17 @@
-const btnAdicionarTarefa = document.getElementById('btn-adicionar-tarefa');
-let listaTarefas = document.getElementById('tarefas-lista');
+document.addEventListener('DOMContentLoaded', () => {
+    const btnAdicionarTarefa = document.getElementById('btn-adicionar-tarefa');
+    let listaTarefas = document.getElementById('tarefas-lista');
+
+    btnAdicionarTarefa.addEventListener('click', () => {
+        const tarefaTexto = prompt('Digite a nova tarefa:');
+        if (tarefaTexto) {
+            adicionarNovaTarefa(tarefaTexto);
+        }
+    })
+
+    //função apenas para exemplificar
+    function adicionarNovaTarefa(tarefa) {
+        listaTarefas.innerHTML += `<span>${tarefa}</span><br>`;
+        alert(`A tarefa ${tarefa} foi adicionada com sucesso!`);
+    }
+})
