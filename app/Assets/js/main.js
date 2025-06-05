a64dca9cd4f42f6e53f6b5e13071570ff38eacce
Ricardo-Donato <pdonatodasilva@gmail.com>
2025-06-04 21:28:43 -0300

    Cria função mostrarMensagem e não aplica ela ainda

--- app/Assets/js/main.js
+++ app/Assets/js/main.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     })
 
-    //função apenas para exemplificar
+    //funï¿½ï¿½o apenas para exemplificar
     function adicionarNovaTarefa(tarefa) {
         listaTarefas.innerHTML += `
             <div class="checkbox-wrapper-13">
@@ -18,5 +18,23 @@ document.addEventListener('DOMContentLoaded', () => {
             </div>`;
 
         alert(`A tarefa ${tarefa} foi adicionada com sucesso!`);
+    }
+
+    function mostrarMensagem(mensagem, type) {
+        let elementoMensagem = document.getElementById('message-container');
+
+        if (!elementoMensagem) {
+            elementoMensagem = document.createElement('div');
+            elementoMensagem.id = 'message-container';
+            document.body.appendChild(elementoMensagem);
+        }
+        elementoMensagem.innerHTML = mensagem;
+        elementoMensagem.className = `message ${type}`;
+        elementoMensagem.style.display = 'block';
+
+        setTimeout(() => {
+            elementoMensagem.style.display = 'none';
+        }, 5000);
+
     }
 })
